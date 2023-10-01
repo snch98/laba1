fc16180f8887dd94836294d5ac79abff20649083
sophiachekmenyova <chekmenova184@gmail.com>
2023-10-01 19:17:24 +0300

    second commit

--- app/src/main/java/com/example/laba1/MainActivity.java
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -12,12 +12,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView laba1 = findViewById(R.id.TextViewLaba1);
-        Button buttonTextChange = findViewById(R.id.TextChangeButton);
+        Button TextChange = findViewById(R.id.TextChangeButton);
 
-        buttonTextChange.setOnClickListener(new View.OnClickListener() {
+        TextChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 laba1.setText("New text is created");
+            }
+        });
+
+        Button TextColorChange = findViewById(R.id.TextColorChangeButton);
+        TextColorChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                laba1.setTextColor(getResources().getColor(R.color.black));
             }
         });
 
